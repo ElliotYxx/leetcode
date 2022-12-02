8b1cf06abec7486b3ce213d53f3b45373101f352
Elliot <1049621663@qq.com>
2022-12-02 19:30:37 +0800

    commit

---
+++ 2022-12-02/FirstUniqueChar.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * https://leetcode.com/problems/first-unique-character-in-a-string/
+ * Given a string s, find the first non-repeating character in it and return its
+ * index. If it does not exist, return -1.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: s = "leetcode"
+ * Output: 0
+ * Example 2:
+ * 
+ * Input: s = "loveleetcode"
+ * Output: 2
+ * Example 3:
+ * 
+ * Input: s = "aabb"
+ * Output: -1
+ * 
+ * 
+ * Constraints:
+ * 
+ * 1 <= s.length <= 105
+ * s consists of only lowercase English letters.
+ */
+class FirstUniqueChar {
+    public static int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("leetcode"));
+    }
+}
